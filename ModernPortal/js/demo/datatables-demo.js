d34dd5892c875debbc62631dfdd98b24f068f1f6
Mark Thekkethala <markthekkethala@Marks-MacBook-Pro.local>
2022-09-12 15:56:56 -0700

    reading url params for submission id modern portal

--- ModernPortal/js/demo/datatables-demo.js
+++ ModernPortal/js/demo/datatables-demo.js
@@ -5,11 +5,18 @@ $(document).ready(function () {
     t.row.add([document.getElementById("input-caseId").value, document.getElementById("input-SSN").value,document.getElementById("input-date").value,document.getElementById("input-transactionType").value,document.getElementById("input-accountType").value,document.getElementById("input-transactionAmount").value,document.getElementById("input-financialAdvisor").value]).draw(false);
     $("#newRecordModal").modal('hide');
   });
+
+  $('#btn-submitEntries').on('click', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    alert('PUT INTO API CALL + ID:' + urlParams.get('submissionId'));
+  });
+
 });
 
 function loginValidation() {
   if (document.getElementById("loginInputEmail").value === ('mark@pixiebrix.com') && document.getElementById('loginInputPassword').value === ('aabotgames2022')) {
-    window.location = "index.html"
+    let userInputSubmissionId = document.getElementById("loginInputSubmissionId").value
+    window.location = "index.html?submissionId="+ userInputSubmissionId;
   } else {
     alert("Incorrect username and/or password.");
   }
