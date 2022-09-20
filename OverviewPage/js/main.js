134de040d9db771ef2f55b49d1439581b9865800
Mark Thekkethala <markthekkethala@Marks-MacBook-Pro.local>
2022-09-19 21:11:54 -0700

    added modern portal button function

--- OverviewPage/js/main.js
+++ OverviewPage/js/main.js
@@ -1,3 +1,7 @@
 document.getElementById('oldPortalLogin').onclick = function (){
     window.location.href = '../../OldPortal/login.html'
 };
+
+document.getElementById('newPortalLogin').onclick = function (){
+    window.location.href = '../../ModernPortal/login.html'
+};
